07fe37a01f7875bcb07f92f0463f1513a5d211b5
Geoff Reedy <geoff@programmer-monk.net>
2022-12-06 08:39:56 -0700

    solve day 6 part 2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -7,26 +7,55 @@ struct Cli {
     input: InputCLI<6>
 }
 
-fn all_distinct<T:PartialEq>(a: T, b: T, c: T, d: T) -> bool {
-    if a == b { return false; }
-    if a == c { return false; }
-    if a == d { return false; }
-    if b == c { return false; }
-    if b == d { return false; }
-    if c == d { return false; }
-    true
+struct Windows<'a, T>{
+    inner: &'a [T],
+    window_size: usize,
+    position: usize
+}
+
+impl<'a, T> Windows<'a, T> {
+    pub fn new(inner: &'a [T], window_size: usize) -> Self {
+        Windows { inner, window_size, position: 0 }
+    }
+}
+
+impl<'a, T> Iterator for Windows<'a, T> {
+    type Item = &'a [T];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.position > self.inner.len() { return None; }
+        let answer = self.inner.get(self.position..(self.position + self.window_size));
+        self.position += self.window_size;
+        answer
+    }
+}
+
+trait WindowsIterExt<T> {
+    fn windows(&self, n: usize) -> Windows<T>;
+}
+
+impl<T> WindowsIterExt<T> for [T] {
+    fn windows(&self, n: usize) -> Windows<T> {
+        Windows::new(self, n)
+    }
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let cli = Cli::parse();
 
-    if let Some(position) = process_results(
-        cli.input.get_input()?.bytes(),
-        |it| it.map_into::<char>().tuple_windows().inspect(|t| println!("Checking window {:?}", t)).position(|(a,b,c,d)| all_distinct(a,b,c,d)))? {
-        println!("Start marker is at position {}", position + 4);
+    let input : Vec<_> = cli.input.get_input()?.bytes().try_collect()?;
+
+    if let Some(position) = input.windows(4).position(|w| w.iter().all_unique()) {
+        println!("Part 1: Start marker is at position {}", position + 4);
     } else {
-        println!("No start marker found!");
+        println!("Part 1: No start marker found!");
+    }
+
+    if let Some(position) = input.windows(14).position(|w| w.iter().all_unique()) {
+        println!("Part 2: Start marker is at position {}", position + 14);
+    } else {
+        println!("Part 2: No start marker found!");
     }
 
     Ok(())
