c82633695b9ad9019796150bce86809eaefc7a9c
Geoff Reedy <geoff@programmer-monk.net>
2022-12-23 13:19:10 -0700

    solve day 20 part 2

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,4 +1,4 @@
-use std::ops::Index;
+use std::{ops::Index, fmt::Display};
 
 use aoc2022::prelude::*;
 use itertools::Itertools;
@@ -11,13 +11,19 @@ struct Cli {
 
 #[derive(Debug)]
 struct Num {
-    data: i32,
+    data: i64,
     prev: isize,
     next: isize,
 }
 
 #[derive(Debug)]
 struct File(Vec<Num>);
+
+impl Display for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", std::iter::successors(Some(0), |ix:&isize| Some(self.at(*ix).next)).map(|ix| self.at(ix).data).take(self.0.len()).join(", "))
+    }
+}
 
 impl File {
     fn advance(&mut self, a: isize) {
@@ -50,18 +56,21 @@ impl File {
     }
 
     fn mix(&mut self) {
+        //println!("Before mixing: {}", &self);
         for orig_ix in 0..self.0.len() {
             let orig_ix: isize = orig_ix.try_into().expect("indices fit in isize");
             let data = self.at(orig_ix).data;
+            let data_abs: usize = data.unsigned_abs().try_into().unwrap();
             if data < 0 {
-                for _ in 0..-data {
+                for _ in 0..(data_abs % (self.0.len() - 1)) {
                     self.retreat(orig_ix);
                 }
-            } else if data > 0 {
-                for _ in 0..data {
+            } else {
+                for _ in 0..(data_abs % (self.0.len() - 1)) {
                     self.advance(orig_ix);
                 }
             }
+            //println!("Round {}: {}", orig_ix+1, &self);
         }
     }
 }
@@ -70,9 +79,10 @@ fn main() -> Result<()> {
     color_eyre::install()?;
     let cli = Cli::parse();
 
-    let numbers: Vec<_> = cli.input.get_input()?.lines().into_eyre().map_and_then(|s| s.parse::<i32>().map_err(Into::into)).try_collect()?;
+    let numbers: Vec<_> = cli.input.get_input()?.lines().into_eyre().map_and_then(|s| s.parse::<i64>().map_err(Into::into)).try_collect()?;
 
-    let mut file = File(numbers.into_iter().zip(0isize..).map(|(data, ix)| Num { data, next: ix + 1, prev: ix - 1 }).collect_vec());
+    {
+    let mut file = File(numbers.iter().copied().zip(0isize..).map(|(data, ix)| Num { data, next: ix + 1, prev: ix - 1 }).collect_vec());
 
     println!("input has {} numbers", file.0.len());
 
@@ -83,7 +93,7 @@ fn main() -> Result<()> {
 
     let mut ii = std::iter::successors(Some(zero_ix), |ix:&isize| Some(file.at(*ix).next)).map(|ix| file.at(ix).data);;
 
-    println!("Mixed list is {}", ii.clone().take(file.0.len()*2).join(", "));
+    //println!("Mixed list is {}", ii.clone().take(file.0.len()*2).join(", "));
 
     let first = ii.nth(1000).unwrap();
     let second = ii.nth(999).unwrap();
@@ -92,6 +102,45 @@ fn main() -> Result<()> {
     dbg!(first, second, third);
 
     println!("Result is {}", first+second+third);
+    }
+
+    {
+
+    let key = 811589153;
+
+    let mut file = File(numbers.iter().copied().map(|x| x  * key).zip(0isize..).map(|(data, ix)| Num { data, next: ix + 1, prev: ix - 1 }).collect_vec());
+
+    //println!("{}", std::iter::successors(Some(0), |ix:&isize| Some(file.at(*ix).next)).map(|ix| file.at(ix).data).take(file.0.len()).join(", "));
+    for i in 0..10 {
+        println!("mixing round {}", i+1);
+        file.mix();
+        //println!("{}", std::iter::successors(Some(0), |ix:&isize| Some(file.at(*ix).next)).map(|ix| file.at(ix).data).take(file.0.len()).join(", "));
+    }
+    let zero_ix:isize = file.0.iter().position(|n| n.data == 0).expect("file to contain a zero").try_into().expect("indicies to fit in isize");
+
+
+    let mut ii = std::iter::successors(Some(zero_ix), |ix:&isize| Some(file.at(*ix).next)).map(|ix| file.at(ix).data);
+
+    //println!("Mixed list is {}", ii.clone().take(file.0.len()*2).join(", "));
+
+    let first = ii.nth(1000).unwrap();
+    let second = ii.nth(999).unwrap();
+    let third = ii.nth(999).unwrap();
+
+    dbg!(-1623178306 / 6);
+    dbg!(-1623178306 % 6);
+
+    dbg!(first, second, third);
+
+    println!("Result is {}", first+second+third);
+    }
 
     Ok(())
 }
+
+// a, b, c, d, e, f
+// b, a, c, d, e, f
+// b, c, a, d, e, f
+// b, c, d, a, e, f
+// b, c, d, e, a, f
+// b, c, d, e, f, a
